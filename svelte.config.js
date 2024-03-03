14b67cc73b57db6441bd2a6a2c9981b90240da89
tinykite <dakota@tinykitelab.com>
2024-03-03 12:44:33 -0600

    feat: initial commit

---
+++ svelte.config.js
@@ -0,0 +1,31 @@
+import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { mdsvex, escapeSvelte } from 'mdsvex'
+import { getHighlighter } from 'shiki'
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await getHighlighter({
+				themes: ['poimandres'],
+				langs: ['javascript', 'typescript']
+			})
+			await highlighter.loadLanguage('javascript', 'typescript')
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
+			return `{@html \`${html}\` }`
+		}
+	},
+}
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte', '.md'],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+	kit: {
+		adapter: adapter()
+	}
+}
+
+export default config
