2d3ba9bd1218d82fc78e95a5abc9632ef7187fa3
tinykite <dakota@tinykitelab.com>
2024-04-25 12:18:02 -0500

    Add aliases to svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,21 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import { mdsvex, escapeSvelte } from 'mdsvex'
-import { getHighlighter } from 'shiki'
-
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-	extensions: ['.md'],
-	highlight: {
-		highlighter: async (code, lang = 'text') => {
-			const highlighter = await getHighlighter({
-				themes: ['poimandres'],
-				langs: ['javascript', 'typescript']
-			})
-			await highlighter.loadLanguage('javascript', 'typescript')
-			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
-			return `{@html \`${html}\` }`
-		}
-	},
-}
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+	preprocess: [vitePreprocess(), mdsvex({
+		extensions: ['.md', '.svx']
+	})],
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: 'src/components',
+			$utils: 'src/utils',
+			$consts: 'src/constants',
+			$data: 'src/data'
+		}
 	}
 }
 
