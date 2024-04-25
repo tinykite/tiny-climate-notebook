7c4c71ba9fee873194f47c8e9c6cdef116523b67
tinykite <dakota@tinykitelab.com>
2024-04-25 16:35:15 -0500

    Remove commented out code

--- src/components/ChartLoader.svelte
+++ src/components/ChartLoader.svelte
@@ -13,36 +13,8 @@
             console.log(err)
         }
     }
-
     fetchData()
-    // async function fetchData() {
-    //   resp.isLoading = true;
-    //   try {
-    //     const response = await fetch("https://randomuser.me/api/?results=3");
-    //     resp.users = (await response.json()).results;
-    //     resp.error = undefined;
-    //   } catch (err) {
-    //     resp.error = err;
-    //     resp.users = undefined;
-    //   }
-    //   resp.isLoading = false;
-    // }
-
-//     import type { DSVRowArray } from "d3";
-
-//     onMount(async () => 
-//     {
-//     const data: DSVRowArray<string> | undefined = await d3.csv(dataSource
-//     );
-
-//     if (!data) {
-//       throw error(500, "No data found");
-//     }
-
-//     console.log(dataset);
-//   });
+   
 </script>
-
-<!-- <ScatterPlot {data} xAccessor={"grade"} yAccessor={"hours"} /> -->
 
 {@render children()}
