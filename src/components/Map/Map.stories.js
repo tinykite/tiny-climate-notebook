73174521cdf5dee3052468d3f350c59fb86e6624
tinykite <dakota@tinykitelab.com>
2024-04-26 10:07:40 -0500

    Add map example from Layercake

---
+++ src/components/Map/Map.stories.js
@@ -0,0 +1,12 @@
+import MapExample from './MapExample.svelte'
+
+export default {
+  component: Map,
+  title: 'Map',
+};
+
+const Template = () => ({
+  Component: MapExample,
+});
+
+export const Default = Template.bind({});
